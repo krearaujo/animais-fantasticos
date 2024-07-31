64f19cc925f3d9b4bcd7ee470b19069afd0d1803
krearaujo <karenohanaaraujo@gmail.com>
2024-07-31 10:53:37 -0300

    dpropdown-menu styles and logic

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
 // import Modal from './modules/modal.js';
 // import Tooltip from './modules/tooltip.js';
-// import DropdownMenu from './modules/dropdown-menu.js';
+import DropdownMenu from './modules/dropdown-menu.js';
 // import MenuMobile from './modules/menu-mobile.js';
 // import Funcionamento from './modules/funcionamento.js';
 // import FetchAnimais from './modules/fetch-animais.js';
@@ -20,3 +20,6 @@ accordion.init();
 
 const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
 tabNav.init();
+
+const dropdrownMenu = new DropdownMenu('[data-dropdown');
+dropdrownMenu.init();
